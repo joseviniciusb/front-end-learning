5e86f80e307478158f48a07305046211e60e45f1
José Vinicius <josevbluz@outlook.com>
2022-02-17 13:07:57 -0300

    describe added

--- react-testing-library/testes-react-app/src/App.test.js
+++ react-testing-library/testes-react-app/src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Verifica se o texto "Testando por texto!!" aparece na tela', () => {
-  render(<App />);
-  const h1Test = screen.getByText('Testando por texto!!');
-  expect(h1Test).toBeInTheDocument();
-});
+
+describe('Alguns exemplos de testes básicos que você pode usar' , () => {
+  test('Verifica se o texto "Testando por texto!!" aparece na tela', () => {
+    render(<App />);
+    const h1Test = screen.getByText('Testando por texto!!');
+    expect(h1Test).toBeInTheDocument();
+  });
+
+})
