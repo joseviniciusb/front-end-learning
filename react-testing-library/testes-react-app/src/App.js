7fa8c39d3d14d66517db64372ed5840bb18fb48d
José Vinicius <josevbluz@outlook.com>
2022-02-22 14:43:48 -0300

    testing in react

--- react-testing-library/testes-react-app/src/App.js
+++ react-testing-library/testes-react-app/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Testando por texto!!</h1>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    carregando: false,
+  };
+
+  loading = () => {
+    this.setState({ carregando: true });
+    setTimeout(() => {
+      console.log('Hello, World!');
+    }, 3000);
+    this.setState({ carregando: false });
+  };
+
+  render() {
+    const loading = this.state;
+    return (
+      <div className="App">{!loading && <h1>Testando por texto!!</h1>}</div>
+    );
+  }
 }
-
-export default App;
--- react-testing-library/testes-react-app/src/App.test.js
+++ react-testing-library/testes-react-app/src/App.test.js
@@ -5,8 +5,8 @@ import App from './App';
 describe('Alguns exemplos de testes básicos que você pode usar' , () => {
   test('Verifica se o texto "Testando por texto!!" aparece na tela', () => {
     render(<App />);
-    const h1Test = screen.getByText('Testando por texto!!');
-    expect(h1Test).toBeInTheDocument();
+    const testByText = screen.getByText('Testando por texto!!');
+    expect(testByText).toBeInTheDocument();
   });
 
 })
