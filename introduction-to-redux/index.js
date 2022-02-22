9fc90594ab5dab9d840cce90bd5c2e8a59797085
José Vinicius <josevbluz@outlook.com>
2022-02-22 15:39:55 -0300

    change state with dispatch

--- introduction-to-redux/index.js
+++ introduction-to-redux/index.js
@@ -1,14 +1,28 @@
 const Redux = require('redux');
 
-const INITIAL_STATE = {
-    login: false,
-    email: "",
-    };
-    
+const fazerLogin = (email) => ({
+  type: 'LOGIN',
+  email,
+});
 
-const reducer = (state = INITIAL_STATE) => {
-    return state;
+const INITIAL_STATE = {
+  login: false,
+  email: '',
 };
 
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        login: !state.login,
+        email: action.email,
+      };
+    default:
+      return state;
+  }
+};
 const store = Redux.createStore(reducer);
-  
+
+store.dispatch(fazerLogin('zezinho@email.com'));
+console.log(store.getState());
