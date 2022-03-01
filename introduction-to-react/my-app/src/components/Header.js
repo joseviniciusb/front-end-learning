ae64030df6337eabb01b4966fc58812dbf2efbae
José Vinicius <josevbluz@outlook.com>
2022-03-01 13:41:30 -0300

    traning JSX with react

---
+++ introduction-to-react/my-app/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>Zezin</h1>
+        <ul className="menu">
+          <li>Home</li>
+          <li>Contact</li>
+          <li>Login</li>
+        </ul>
+      </header>
+    </>
+  );
+};
+
+export default Header;
