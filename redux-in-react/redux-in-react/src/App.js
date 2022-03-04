defb8cfab3aa369dd7882ccea6d97bd1a952321c
José Vinicius <josevbluz@outlook.com>
2022-03-04 19:08:53 -0300

    data of films added

---
+++ redux-in-react/redux-in-react/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <header className="Header">
+      <h1>ZezinFlix</h1>
+    </header>
+  );
+}
+
+export default App;
---
+++ redux-in-react/redux-in-react/src/data/data.js
@@ -0,0 +1,20 @@
+const categories = [
+  {
+    id: 1,
+    name: 'Fantasia',
+    movies: [
+      {
+        id: 1,
+        tittle: 'Zezé Potter e a pedra secreta',
+        released: 2001,
+        link: 'https://www.youtube.com/watch?v=mNgwNXKBEW0',
+      },
+      {
+        id: 2,
+        tittle: 'Zezão Potter e a câmara filosofal',
+        released: 2003,
+        link: 'https://www.youtube.com/watch?v=1bq0qff4iF8',
+      },
+    ],
+  },
+];
