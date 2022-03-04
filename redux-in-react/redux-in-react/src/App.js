80d698c5575ea906bfcb892d0a1eff5abeb67128
José Vinicius <josevbluz@outlook.com>
2022-03-04 19:23:20 -0300

    initial Css done

--- redux-in-react/redux-in-react/src/App.js
+++ redux-in-react/redux-in-react/src/App.js
@@ -2,9 +2,26 @@ import './App.css';
 
 function App() {
   return (
+      <>
     <header className="Header">
       <h1>ZezinFlix</h1>
     </header>
+    <main>
+    <div className="moviesContainer">
+      <h2>Um filme qualquer</h2>
+      <h3>Comédia - 2019</h3>
+      <div className="trailerContainer"></div>
+    </div>
+    <div className="categoriesContainer">
+    <h2>Escolha uma categoria</h2>
+      <ul>
+        <li>
+         Um filme qualquer was released in 1995
+        </li>
+      </ul>
+    </div>
+    </main>
+    </>
   );
 }
 
