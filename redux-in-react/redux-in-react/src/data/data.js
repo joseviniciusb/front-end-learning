8b2d76c239368e363f4452c722db218104f4e669
José Vinicius <josevbluz@outlook.com>
2022-03-04 19:12:48 -0300

    data increased

--- redux-in-react/redux-in-react/src/data/data.js
+++ redux-in-react/redux-in-react/src/data/data.js
@@ -17,4 +17,25 @@ const categories = [
       },
     ],
   },
+  {
+    id: 1,
+    name: 'Animação',
+    movies: [
+      {
+        id: 1,
+        tittle: 'Toy Story',
+        released: 1997,
+        link: 'https://www.youtube.com/watch?v=4KPTXpQehio',
+      },
+      {
+        id: 2,
+        tittle: 'Monstros S.A',
+        released: 2003,
+        link: 'https://www.youtube.com/watch?v=iRh2kF-1X2E',
+      },
+    ],
+  },
 ];
+
+
+console.log(categories)
